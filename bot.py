28bab3be8bd450264c898f7469ad014828fcd729
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2025-07-19 21:58:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ class Bot(Client):
     async def start(self):
             
         await super().start()
-        print('Bot Started Powered By @VJ_Botz')
+        print('Bot Started Powered By @xspes')
 
     async def stop(self, *args):
 
